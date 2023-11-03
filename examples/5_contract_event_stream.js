0b6f074f513f46b42cacda5cb8694c4da294f8a6
CoorayNTL <tl685998@gmail.com>
2023-11-04 02:48:02 +0530

    tx inside the block

--- examples/5_contract_event_stream.js
+++ examples/5_contract_event_stream.js
@@ -3,13 +3,15 @@ const { ethers } = require("ethers");
 const INFURA_ID = ''
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
+//ERC20 ABI https:
+//https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
 const ERC20_ABI = [
     "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
     "function balanceOf(address) view returns (uint)",
 
-    "event Transfer(address indexed from, address indexed to, uint amount)"
+    "event Transfer(address indexed from, address indexed to, uint amount)" //solidity event 
 ];
 
 const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
@@ -18,7 +20,9 @@ const contract = new ethers.Contract(address, ERC20_ABI, provider)
 const main = async () => {
     const block = await provider.getBlockNumber()
 
-    const transferEvents = await contract.queryFilter('Transfer', block - 1, block)
+    //contract.queryFilder is a function that takes in the event name and the block range
+    const transferEvents = await contract.queryFilter('Transfer', block - 1, block) //https://docs.ethers.org/v5/api/contract/contract/ 
+    
     console.log(transferEvents)
 }
 

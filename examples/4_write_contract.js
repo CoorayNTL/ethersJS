10a67d2ae0ac73b9a36c20af910811ff6f09e884
CoorayNTL <tl685998@gmail.com>
2023-11-04 02:28:20 +0530

    write smart contracts

--- examples/4_write_contract.js
+++ examples/4_write_contract.js
@@ -3,15 +3,18 @@ const { ethers } = require("ethers");
 const INFURA_ID = ''
 const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
+const account1 = '' // my account address 1
+const account2 = '' // sender account address 2
 
-const privateKey1 = '' // Private key of account 1
-const wallet = new ethers.Wallet(privateKey1, provider)
+const privateKey1 = '' // my key of account 1 basically metamask provoded key don't put in your real key(ETH availebele key) 
+//don't use this private key any one catch owner passwaord 
+
+const wallet = new ethers.Wallet(privateKey1, provider) //https://docs.ethers.org/v4/api-wallet.html
+
 
 const ERC20_ABI = [
     "function balanceOf(address) view returns (uint)",
-    "function transfer(address to, uint amount) returns (bool)",
+    "function transfer(address to, uint amount) returns (bool)", // this is the function we want to call , this tx happen one wallet to another wallet
 ];
 
 const address = ''

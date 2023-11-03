38fd4e357da19dd61b3266529e1d51b39d5ed14a
CoorayNTL <tl685998@gmail.com>
2023-11-04 02:51:50 +0530

    get the block with transactions

--- examples/6_inspecting_blocks.js
+++ examples/6_inspecting_blocks.js
@@ -1,21 +1,24 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const INFURA_ID = "";
+const provider = new ethers.providers.JsonRpcProvider(
+    `https://mainnet.infura.io/v3/${INFURA_ID}`
+);
 
 const main = async () => {
-    const block = await provider.getBlockNumber()
+    const block = await provider.getBlockNumber(); //get the block number of the latest block
 
-    console.log(`\nBlock Number: ${block}\n`)
+    console.log(`\nBlock Number: ${block}\n`);
 
-    const blockInfo = await provider.getBlock(block)
+    const blockInfo = await provider.getBlock(block); //get the block info of the latest block
 
-    console.log(blockInfo)
+    console.log(blockInfo);
 
-    const { transactions } = await provider.getBlockWithTransactions(block)
+    //get the block with transactions
+    const { transactions } = await provider.getBlockWithTransactions(block); //https://docs.ethers.org/v5/api/providers/provider/
 
-    console.log(`\nLogging first transaction in block:\n`)
-    console.log(transactions[0])
-}
+    console.log(`\nLogging first transaction in block:\n`);
+    console.log(transactions[0]);
+};
 
-main()
+main();
